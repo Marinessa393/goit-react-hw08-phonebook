d6d80e6b91b96167f7ac230444a7d3b44835a996
Marinessa393 <futurism1303@gmail.com>
2021-04-12 22:02:07 +0300

    create contacts view

---
+++ src/views/ContactsView.js
@@ -0,0 +1,21 @@
+import ContactForm from '../components/ContactForm';
+import ContactList from '../components/ContactList';
+import Filter from '../components/Filter';
+
+export const ContactsView = () => {
+  return (
+    <>
+      <div className="Container">
+        <section title="Phonebook" className="Phonebook__section">
+          <h1 className="Phonebook__title">Phonebook</h1>
+          <ContactForm />
+        </section>
+        <section title="Contacts" className="Contacts">
+          <h2 className="Contacts__title">Contacts</h2>
+          <Filter />
+          <ContactList />
+        </section>
+      </div>
+    </>
+  );
+};
