43bf141029197480ae47ea9a57b7531f9e688df6
Marinessa393 <futurism1303@gmail.com>
2021-04-12 22:02:52 +0300

    add routes

---
+++ src/App/App.js
@@ -0,0 +1,23 @@
+import { Route, Switch } from 'react-router-dom';
+import { ContactsView } from '../views/ContactsView';
+import { MainPageView } from '../views/MainPageView';
+import { RegisterView } from '../views/RegisterView';
+import { LoginView } from '../views/LoginView';
+import { AppBar } from '../components/AppBar';
+// import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <AppBar />
+      <Switch>
+        <Route exact path="/" component={MainPageView} />
+        <Route exact path="/register" component={RegisterView} />
+        <Route exact path="/login" component={LoginView} />
+        <Route exact psth="/contacts" component={ContactsView} />
+      </Switch>
+    </>
+  );
+};
+
+export default App;
