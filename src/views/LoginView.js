e0e9ac8007cae173d652ad109816a58b6dc00a3a
Marinessa393 <futurism1303@gmail.com>
2021-04-12 22:00:49 +0300

    add login form

---
+++ src/views/LoginView.js
@@ -0,0 +1,46 @@
+import { Component } from 'react';
+
+export class LoginView extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  };
+
+  handleSumbit = e => {
+    e.preventDefault();
+    this.setState({ name: '', email: '', password: '' });
+  };
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <>
+        <form onSubmit={this.handleSumbit}>
+          <label>
+            <p>E-mail:</p>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            <p>Password:</p>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Login</button>
+        </form>
+      </>
+    );
+  }
+}
